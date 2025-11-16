6e4973efcc42456ed4f877b8ba2c0b3a592570dd
anali <anali@Famlia>
2025-11-15 21:58:07 -0300

    inclui, editei e removi midia no gerenciadorMidiaController.

---
+++ src/main/java/GerenciadorMidiaController/GerenciadorController.java
@@ -0,0 +1,5 @@
+package GerenciadorMidiaController;
+
+public class GerenciadorController {
+
+}
--- src/main/java/GerenciadorMidiaModel/Musica.java
+++ src/main/java/GerenciadorMidiaModel/Musica.java
@@ -2,6 +2,11 @@ package GerenciadorMidiaModel;
 
 public class Musica extends Midia {
 	protected String artista;
+	
+	public Musica(String local, long tamanho, String titulo, int duracao, String categoria, String artista) {
+		super(local, tamanho, titulo, duracao, categoria);
+		this.artista = artista;
+	}
 
 	public String getArtista() {
 		return artista;
@@ -9,6 +14,10 @@ public class Musica extends Midia {
 
 	public void setArtista(String artista) {
 		this.artista = artista;
+	}
+	@Override
+	public String exibirAtributos() {
+		return "Música: " + getTitulo() + " | Artista: " + artista + " | Duração: " + getDuracao() + "seg | Categoria: " + getCategoria();
 	}
 	
 
